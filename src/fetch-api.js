ae0030561f313829846aa2cca4218fa2d7659056
Vladislav-UZH <perebziak.vladislav@gmail.com>
2022-11-20 00:31:18 +0200

    start hw

---
+++ src/fetch-api.js
@@ -0,0 +1,13 @@
+const API_LINK = 'https://restcountries.com'
+
+function onFetchData(countries) {
+    return fetch(`${API_LINK}/v3.1/name/${countries}?fields=name,capital,population,flags,languages`)
+        .then(r => {
+            if (!r.ok) {
+                // throw new Error(r.status);
+            };
+            return r.json();
+        })
+        .catch(err=> console.error(err))
+}
+export default onFetchData;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
-
+import Notiflix from "notiflix";
+import onFetchData from "./fetch-api";
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+
+// Notiflix
+const refs = {
+    inputEl: document.querySelector('#search-box'),
+    searchCountryListEl: document.querySelector('.country-list')
+
+}
+
+refs.inputEl.addEventListener('input',onEntering)
+function onEntering(e) {
+    const inputValue = e.target.value;
+    console.log();
+    onFetchData(inputValue.trim()).then(data => {
+   checkArrLength(data)
+})
+}
+
+console.log('one');
+
+
+function checkArrLength(data) {
+    if (data.length > 10) {
+        console.log(data.length);
+       return Notiflix.Notify.warning("Too many matches found. Please enter a more specific name.")
+    } else if (!!~data.length) {
+        
+        Notiflix.Notify.failure("Oops, there is no country with that name")
+    }
+     return createMarkup(data);
+    }
+   
+
+function createMarkup(data) {
+    const markup = data.map(el => `
+    <li><img src="${el.flags.svg}" width="35px" height="50px" alt="flag of ${el.name.common} "><span>${el.name.common}</span></li>`)
+        .join('');
+    refs.searchCountryListEl.innerHTML = markup;
+}
